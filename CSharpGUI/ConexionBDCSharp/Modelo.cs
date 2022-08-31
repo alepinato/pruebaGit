926724468d165aac3b590e74259c9922c7824e87
alepinato <prof.ampinato@gmail.com>
2022-08-31 11:07:38 -0300

    31/09

---
+++ CSharpGUI/ConexionBDCSharp/Modelo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace ConexionBDCSharp
+{
+    class Modelo
+    {
+        public int registroUsu(Usuarios usu)
+        {
+            MySqlConnection conexion = Conexion.getConexion();
+            conexion.Open();
+
+            string sql = "INSERT INTO usuarios(usuario, password, tipo_usuario) VALUES(@usuario, @password,@tipo_usuario)";
+
+            MySqlCommand cmd = new MySqlCommand(sql, conexion);
+            cmd.Parameters.AddWithValue("@usuario", usu.Usuario);
+            cmd.Parameters.AddWithValue("@password", usu.Password);
+            cmd.Parameters.AddWithValue("@tipo_usuario", 1);
+
+            int resultado = cmd.ExecuteNonQuery();
+
+            return resultado;
+
+
+        }
+
+    }
+}
---
+++ CSharpGUI/ConexionBDCSharp/Usuarios.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConexionBDCSharp
+{
+    public class Usuarios
+    {
+        int id, tipo_usuario;
+        string usuario, password, conpassword;
+
+        public string Conpassword
+        {
+            get
+            {
+                return conpassword;
+            }
+
+            set
+            {
+                conpassword = value;
+            }
+        }
+
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+
+            set
+            {
+                id = value;
+            }
+        }
+
+        public string Password
+        {
+            get
+            {
+                return password;
+            }
+
+            set
+            {
+                password = value;
+            }
+        }
+
+        public int Tipo_usuario
+        {
+            get
+            {
+                return tipo_usuario;
+            }
+
+            set
+            {
+                tipo_usuario = value;
+            }
+        }
+
+        public string Usuario
+        {
+            get
+            {
+                return usuario;
+            }
+
+            set
+            {
+                usuario = value;
+            }
+        }
+    }
+}
