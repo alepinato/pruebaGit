b74627cef0f237739d422aa78b7afbe78c9e1d37
alepinato <prof.ampinato@gmail.com>
2022-09-06 22:33:01 -0300

    06/09

--- CSharpGUI/ConexionBDCSharp/Form1.Designer.cs
+++ CSharpGUI/ConexionBDCSharp/Form1.Designer.cs
@@ -44,20 +44,20 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(39, 52);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.Size = new System.Drawing.Size(43, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Servidor";
+            this.label1.Text = "Usuario";
             // 
             // txtServidor
             // 
-            this.txtServidor.Location = new System.Drawing.Point(100, 52);
+            this.txtServidor.Location = new System.Drawing.Point(157, 49);
             this.txtServidor.Name = "txtServidor";
             this.txtServidor.Size = new System.Drawing.Size(144, 20);
             this.txtServidor.TabIndex = 1;
             // 
             // txtPuerto
             // 
-            this.txtPuerto.Location = new System.Drawing.Point(100, 88);
+            this.txtPuerto.Location = new System.Drawing.Point(157, 85);
             this.txtPuerto.Name = "txtPuerto";
             this.txtPuerto.Size = new System.Drawing.Size(144, 20);
             this.txtPuerto.TabIndex = 3;
@@ -67,13 +67,13 @@
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(39, 88);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.Size = new System.Drawing.Size(61, 13);
             this.label2.TabIndex = 2;
-            this.label2.Text = "Puerto";
+            this.label2.Text = "Contraseña";
             // 
             // txtUsuario
             // 
-            this.txtUsuario.Location = new System.Drawing.Point(100, 129);
+            this.txtUsuario.Location = new System.Drawing.Point(157, 126);
             this.txtUsuario.Name = "txtUsuario";
             this.txtUsuario.Size = new System.Drawing.Size(144, 20);
             this.txtUsuario.TabIndex = 5;
@@ -83,13 +83,13 @@
             this.label3.AutoSize = true;
             this.label3.Location = new System.Drawing.Point(39, 129);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(43, 13);
+            this.label3.Size = new System.Drawing.Size(107, 13);
             this.label3.TabIndex = 4;
-            this.label3.Text = "Usuario";
+            this.label3.Text = "Confirmar contraseña";
             // 
             // txtpass
             // 
-            this.txtpass.Location = new System.Drawing.Point(100, 170);
+            this.txtpass.Location = new System.Drawing.Point(157, 167);
             this.txtpass.Name = "txtpass";
             this.txtpass.Size = new System.Drawing.Size(144, 20);
             this.txtpass.TabIndex = 7;
@@ -117,7 +117,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(439, 261);
             this.Controls.Add(this.btnConectar);
             this.Controls.Add(this.txtpass);
             this.Controls.Add(this.label4);
--- CSharpGUI/ConexionBDCSharp/Modelo.cs
+++ CSharpGUI/ConexionBDCSharp/Modelo.cs
@@ -11,10 +11,12 @@ namespace ConexionBDCSharp
     {
         public int registroUsu(Usuarios usu)
         {
+            //Establezco conexion y abro la BD
             MySqlConnection conexion = Conexion.getConexion();
             conexion.Open();
 
-            string sql = "INSERT INTO usuarios(usuario, password, tipo_usuario) VALUES(@usuario, @password,@tipo_usuario)";
+            string sql = "INSERT INTO usuarios(usuario, password, tipo_usuario)"+
+                         "VALUES(@usuario, @password,@tipo_usuario)";
 
             MySqlCommand cmd = new MySqlCommand(sql, conexion);
             cmd.Parameters.AddWithValue("@usuario", usu.Usuario);
@@ -22,10 +24,32 @@ namespace ConexionBDCSharp
             cmd.Parameters.AddWithValue("@tipo_usuario", 1);
 
             int resultado = cmd.ExecuteNonQuery();
-
+            
             return resultado;
 
 
+        }
+
+
+        public bool existeUsuario(string usuario)
+        {
+            MySqlDataReader reader;
+            MySqlConnection conexion = Conexion.getConexion();
+            conexion.Open();
+
+            string sql = "SELECT id FROM usuarios WHERE usuario LIKE @usuario";
+            MySqlCommand comando = new MySqlCommand(sql, conexion);
+            comando.Parameters.AddWithValue("@usuario", usuario);
+            reader = comando.ExecuteReader();
+
+            if (reader.HasRows)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
 
     }
